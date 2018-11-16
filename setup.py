bf0f2c578cb01da110bc0b3bc5a19e9c58690066
Trefor Evans <trefor.evans@mail.utoronto.ca>
2018-11-16 17:23:30 -0500

    bump -> v1.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='direct',
-    version='1.0',
+    version='1.1',
     description='Discrete relaxation of continuous variables for tractable variational inference.',
     url='https://github.com/treforevans/direct',
     author='Trefor W. Evans',
